1ddfc851157b81c81906f71ccd2d6e0de6c6e4c6
Steinmann György <steinmanngyorgy@gmail.com>
2021-02-10 09:10:32 +0100

    First test case

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -6,9 +6,15 @@ namespace Shopping_TDD
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Call_RegisterProduct()
         {
-
+            /* create shoppingcart */
+            ShoppingCart Shop = new ShoppingCart();
+            Shop.RegisterProduct('A', 10);
+            Shop.RegisterProduct('B', 20);
+            Shop.RegisterProduct('C', 30);
+            var price = Shop.GetPrice("AC");
+            Assert.Equal(40, price);
         }
     }
 }
