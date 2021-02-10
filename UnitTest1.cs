89aca9d20e74fb73785eaaecc23488f45f891945
Steinmann György <steinmanngyorgy@gmail.com>
2021-02-10 09:41:27 +0100

    Problem2 : Pay N get M discount implemented

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -4,29 +4,71 @@ using Xunit;
 
 namespace Shopping_TDD
 {
+    public class CountDiscount
+    {
+        public double Ratio { get; set; }
+        public CountDiscount(int N, int M)
+        {
+            Ratio = (double)N / M;
+        }
+    }
+
     public class ShoppingCart
     {
-     
+        private Dictionary<char, CountDiscount> countdiscounts = new Dictionary<char, CountDiscount>();
+
         private Dictionary<char, double> products = new Dictionary<char, double>();
-    
+
+        private Dictionary<char, int> shoppinglist = new Dictionary<char, int>();
+
         public void RegisterProduct(char name, double price)
         {
             products.Add(name, price);
         }
+
         public double GetPrice(string s)
         {
             double sum = 0;
-            foreach(char  c in s)
+
+            foreach (char c in s)
             {
-                if(products.ContainsKey(c))
+                if (shoppinglist.ContainsKey(c))
                 {
-                    sum += products[c];
+                    shoppinglist[c] += 1;
+                }
+                else
+                {
+                    shoppinglist[c] = 1;
                 }
             }
+
+            /* apply countdiscount */
+            foreach (KeyValuePair<char, CountDiscount> am_discount in countdiscounts)
+            {
+                if (shoppinglist.ContainsKey(am_discount.Key))
+                {
+                    shoppinglist[am_discount.Key] = (int)Math.Ceiling(shoppinglist[am_discount.Key] * am_discount.Value.Ratio);
+                }
+            }
+
+            foreach (KeyValuePair<char, int> shopitem in shoppinglist)
+            {
+                sum += shopitem.Value * products[shopitem.Key];
+            }
+
             return sum;
         }
 
+
+        
+        public void RegisterCountDiscount(char name, int topay, int number)
+        {
+            countdiscounts[name] = new CountDiscount(topay, number);
+        }
     }
+
+
+
     public class UnitTest1
     {
         [Fact]
